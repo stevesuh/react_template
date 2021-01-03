08782aba16afb2d4d8bfe329ff7e79f854c95f4b
Steve Suh <stevesuh44@gmail.com>
2021-01-03 14:32:20 -0500

    Bootstrap create react app with material-ui.

---
+++ sample-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Button from '@material-ui/core/Button';
+
+function App() {
+  return (
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+  );
+}
+
+export default App;
