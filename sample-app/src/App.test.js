fd257a1efb742a87a580cbef3a0f31f0295e31fb
Steve Suh <stevesuh44@gmail.com>
2021-01-03 15:23:21 -0500

    Update test.

--- sample-app/src/App.test.js
+++ sample-app/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders hello world button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonElement = screen.getByText(/hello world/i);
+  expect(buttonElement).toBeInTheDocument();
 });
