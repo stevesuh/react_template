e5d1b57ea4224344ba19ae0d97f307b5fc0baaef
Steve Suh <stevesuh44@gmail.com>
2021-01-07 21:11:05 -0500

    Convert to typescript template.

--- sample-app/src/App.tsx
+++ sample-app/src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css';
 import Button from '@material-ui/core/Button';
 
+function clicked() {
+    window.alert("Clicked")
+}
+
 function App() {
   return (
-      <Button variant="contained" color="primary">
+      <Button variant="contained" color="primary" onClick={clicked}>
         Hello World
       </Button>
   );
